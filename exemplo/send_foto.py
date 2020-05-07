721c0bbe655c2e0024c9efb93dbe71e54f8b6c38
juan.marinho <juan.marinho@tvglobo.com.br>
2020-05-07 01:34:37 -0300

    Update

--- exemplo/send_foto.py
+++ exemplo/send_foto.py
@@ -1,12 +1,22 @@
-from requests import post 
+from requests import post
 
-fin = open('jn.png', 'rb')
-files = {'file': fin}
-try:
-   print("[*] ENVIANDO FRAME PARA API MODEL 1 - FACE COUNTE")
-   r = post("http://127.0.0.1:5000/file-upload", files=files)
-   print(r.json())
-finally:
-   fin.close()
+#endpoint = "https://IckyBronzeAbstracttype.juanengml.repl.co/file-upload"
 
-
+endpoint = "https://IllLiveRepo.juanengml.repl.co/Upload_img/Contador_faces/"
+while True:
+ fin = open('mulheres.png', 'rb')
+ files = {'file': fin}
+ try:
+    print("[*] ENVIANDO FRAME PARA API MODEL 1 - FACE COUNTE")
+    r = post(endpoint, files=files)
+    print(r.json())
+ finally:
+    fin.close()
+ fin = open('jn.png', 'rb')
+ files = {'file': fin}
+ try:
+    print("[*] ENVIANDO FRAME PARA API MODEL 1 - FACE COUNTE")
+    r = post(endpoint, files=files)
+    print(r.json())
+ finally:
+    fin.close()
